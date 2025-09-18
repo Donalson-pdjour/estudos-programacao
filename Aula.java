78f2d03e28d6f8f3d28dad2eed541bb6a8a531c0
DONALSON POINT DU JOUR <60266734073@senacrs.edu.br>
2025-09-17 21:04:57 -0300

    aula-3

--- Aula.java
+++ Aula.java
@@ -5,23 +5,28 @@ import java.util.Queue;
 
 public class Aula{
     public static void main(String[] args) {
-        Queue<String> Nomes = new LinkedList<>();
-        Nomes.add("Eduardo");
-        Nomes.add("Maria");
-        Nomes.add("João");
-        Nomes.add("Carlos");
-        Nomes.add("Ana");
-        Nomes.add("Julia");
-        Nomes.add("Pedro");
+         Queue<Double> Num = new LinkedList<>();
+        Num.add(1.19);
+        Num.add(3.79);
+        Num.add(2.55);
+        Num.add(4.85);
+        Num.add(99.90);
+        Num.add(1.90);
+        Num.add(3.70);
+        Num.add(5.66);
+        Num.add(3.45);
+        Num.add(7.89);
 
-        System.out.println("Próximo: "+ Nomes.peek());
-        System.out.println("A fila contém: "+Nomes);
-        
-        Nomes.poll();
-        Nomes.poll();
-
-        System.out.println("A fila contém, agora: "+ Nomes);
-        System.out.println("A fila está vazia: "+Nomes.isEmpty());
-
+        System.out.println("A fila contém: "+Num);
+        System.out.println("Próximo a ser removido: "+Num.peek());
+        Num.poll();
+        Num.poll();
+        Num.poll();
+        System.out.println("A fila contém: "+Num);
+        System.out.println("\n Limpando todos itens da fila...");
+        Num.clear();
+        System.out.println("Fila após a limpeza: "+ Num);
+        System.out.println("Fila está vazia: "+Num.isEmpty());
+      
     }
 }
